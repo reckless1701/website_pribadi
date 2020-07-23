9181d3167e482a93c14c501bc6cb27c86d6c552e
Burhanudin Dwi Saputra <dwisputr7@gmail.com>
2020-07-23 14:05:28 +0700

    Added blog_pribadi

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<style>
+    h3{
+        text-align : left;
+        font-size : 17px;
+    }
+    h1{
+        font-size : 32px;
+        text-align : center;
+    }
+</style>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="home">Biodata</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="home2">Experience</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="home3">Portofolio</a>
+            </li>
+        </ul>
+    </div>
+    </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+        <h1 class="display-4" style="font-size: 45px";><b>Biodata Saya<b></h1>
+        <p class="lead" style="font-size: 30px">SMK Taruna Bhakti Depok</p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <h1>About Me</h1>
+        </div>
+    </div>
+<div class="row">
+    <div class="col text-center">
+        <h3>
+            Nama : Burhanudin Dwi Saputra
+            <br>
+            Kelas : XII RPL 2
+            <br>
+            Tempat Tanggal Lahir : Depok 17 Januari 2003
+            <br>
+            Agama : Islam
+            <br>
+            Jenis kelamin : Laki Laki
+            <br>
+            Alamat : Jalan Kenari V Blok i4 No 2 Komp TNI AD 
+            <br>
+            RT/RW : 002/013
+            <br>
+            Kelurahan : Sukamaju Baru
+            <br>
+            Kecamatan : Tapos
+            <br>
+            Kota : Depok
+            <br>
+            Provinsi : Jawa Barat
+            <br>
+            Kewarganegaraan : Indonesia
+            <br>
+            Hobby : Bermain Sepak Bola
+            <br>
+            Motto : Tetaplah berjuang walaupun itu sakit agar dapat menghargai setiap waktu        
+        </h3>
+    </div>
+    <div class="col text-center">
+        <h3>
+            Riwayat Sekolah
+            <br></br>
+            1. TK : TKIT Al-Chasanah
+            <br>
+            Tahun Ajaran : 2008-2009
+            <br>
+            2. SD : SDN RRI NASIONAL
+            <br>
+            Tahun Ajaran : 2009-2015
+            <br>
+            3. SMP : SMP 11 Depok 
+            <br>
+            Tahun Ajaran : 2016-2018
+            <br>
+            4. SMK : SMK Taruna Bhakti Depok
+            <br>
+            Tahun Ajaran : 2018-Sekarang  
+        </h3>
+    </div>
+</div>
+</div>
+</body>
+</html>
---
+++ resources/views/experience.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<style>
+    img{
+        margin : 10px;
+        width: 15%;
+        display : block;
+        margin-left : auto;
+        margin-right : auto;
+    }
+    p{
+        font-size : 20px;
+        font-family : sans-serif;
+
+    }
+
+</style>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="home">Biodata</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="home2">Experience</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="home3">Portofolio</a>
+            </li>
+        </ul>
+    </div>
+    </div>
+</nav>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+        <img src="user.png">
+        <h1 class="display-4" style="font-size: 40px";>Burhanudin Dwi Saputra</h1>
+        <p class="lead" style="font-size: 30px">SMK Taruna Bhakti Depok</p>
+    </div>
+</div>
+
+<div class="container">
+<h2 style= "text-align : center">Experience</h2>
+        <p>
+            Nama Saya Burhanudin Dwi Saputra Dari SMK Taruna Bhakti, Saya sudah dapat memahami beberapa bahasa
+            pemograman. Saya juga mempunyai pengalaman membuat website dan aplikasi desktop. Saya juga pernah mengikuti
+            workshop yang diadakan di sekolah dan juga mengikuti acara starbhak day. Di acara Starbhak Day saya dan kelompok
+            saya membuat aplikasi mobile.
+
+        </p>
+
+</div>
+</body>
+</html>
